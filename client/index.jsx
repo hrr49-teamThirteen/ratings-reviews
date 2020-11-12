fea8fe1081807afb3516e338fc18d54068b21183
Sam Goldie <samuelmgoldie@gmail.com>
2020-11-12 13:59:30 -0800

    Commit everything in prep for migration to own github

---
+++ client/index.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      reviews: []
+    };
+  }
+}
---
+++ src/getReviews.js
@@ -0,0 +1,1 @@
+console.log('Hello World from getReviews main file!');
