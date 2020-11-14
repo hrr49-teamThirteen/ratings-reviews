e1feaa54f33adcef259d1544ce65d47063b41448
Sam Goldie <samuelmgoldie@gmail.com>
2020-11-14 11:45:28 -0800

    Finish seed script

--- database/fake/fakeData.js
+++ database/fake/fakeData.js
@@ -3,18 +3,25 @@ const mysql = require('mysql');
 
 // add more commerce.productAdjective to this string-object?
 const fakeProduct = () => {
-  return new Promise((resolve, reject) => {
-    resolve(newData = faker.fake('{{commerce.color}}, {{commerce.department}}, {{commerce.productName}}, {{commerce.price}}, {{commerce.productAdjective}}, {{commerce.productMaterial}}, {{commerce.product}}, {{image.image}}'));
-  }).then((error, result) => {
-    if (error) {
-      throw error;
-    }
-    result = result.split(',');
-    return {color: result[0], department: result[1], productName: result[2], price: result[3], prodAdj: result[4], prodMaterial: result[5], product: result[6], productImage: result[7]};
-  }).catch(error => {
-    console.error(error);
-  });
+  let newProduct = faker.fake('{{commerce.color}}, {{commerce.department}}, {{commerce.productName}}, {{commerce.price}}, {{commerce.productAdjective}}, {{commerce.productMaterial}}, {{commerce.product}}, {{image.image}}');
+  newProduct = newProduct.split(',');
+  return {color: newProduct[0], department: newProduct[1], productName: newProduct[2], price: newProduct[3], prodAdj: newProduct[4], prodMaterial: newProduct[5], product: newProduct[6], productImage: newProduct[7]};
 };
+
+// const fakeProduct = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve(newData = faker.fake('{{commerce.color}}, {{commerce.department}}, {{commerce.productName}}, {{commerce.price}}, {{commerce.productAdjective}}, {{commerce.productMaterial}}, {{commerce.product}}, {{image.image}}'));
+//   }).then((error, result) => {
+//     if (error) {
+//       throw error;
+//     }
+//     result = result.split(',');
+//     console.log('Result is of type: ' + typeof result);
+//     return {color: result[0], department: result[1], productName: result[2], price: result[3], prodAdj: result[4], prodMaterial: result[5], product: result[6], productImage: result[7]};
+//   }).catch(error => {
+//     console.error(error);
+//   });
+// };
 
 const fakeReview = () => {
   return new Promise((resolve, reject) => {
@@ -33,22 +40,18 @@ const fakeReview = () => {
 };
 
 const fakeData = (n) => {
+  console.log('I MADE IT INTO FAKEDATA!');
   // create an empty array
   let products = [];
   let reviews = [];
   // iterate through from 1 to n
   for (let i = 0; i < n; i++) {
+    console.log('MADE IT INTO THE FOR LOOP!');
     // push a fakeProduct onto the empty array
-    return new Promise((resolve, reject) => {
-
-      resolve({product: fakeProduct(), review: fakeReview()});
-    }).then((error, result) => {
-      if (error) {
-        throw error;
-      }
-      products.push(result.product);
-      reviews.push(result.review);
-    });
+    products.push(fakeProduct());
+    console.log('PRODUCTS IS CURRENTLY: ' + JSON.stringify(products));
+    reviews.push(fakeReview());
+    console.log('REVIEWS IS CURRENTLY: ' + JSON.stringify(reviews));
   }
   // return the initial array
   console.log('HERE ARE THE PRODUCTS: ' + JSON.stringify(products));
@@ -56,7 +59,7 @@ const fakeData = (n) => {
   console.log('HERE IS THE LENGTH OF PRODUCTS: ' + products.length);
   console.log('HERE IS THE LENGTH OF REVIEWS: ' + reviews.length);
   console.log('HERE IS THE TYPE OF AN ITEM IN PRODUCTS: ' + typeof products[0]);
-  console.log('THIS IS THE FIRST ITEM IN PRODUCTS: ' + products[0]);
+  console.log('THIS IS THE FIRST ITEM IN PRODUCTS: ' + JSON.stringify(products[0]));
 
   return {products: products, reviews: reviews};
 };
--- database/fake/index.js
+++ database/fake/index.js
@@ -3,88 +3,31 @@ const fakeData = require('./fakeData.js');
 const faker = require('faker');
 const util = require('util');
 
-const db = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'reviewsDB'
 });
 
-db.connect((err) => {
+connection.connect((err) => {
   if (err) {
-    throw err;
+    console.error(error);
   }
 });
 
-console.log(faker.image.image());
-console.log(faker.commerce.productName());
-console.log(faker.commerce.department());
-
 // don't forget to use ? to escape from sql injections
 
-// let seedFakeData = (n) => {
-//   // create a variable that is equal to a call to fakeData passing in n
-//   const dataSeed = util.promisify(fakeData(n))
-//     .then((result) => {
+const seedFakeData = async function (n) {
+  const dataSeed = fakeData(n);
+  console.log('HERES THE DATASEED: ' + JSON.stringify(dataSeed));
+  for (let product of dataSeed.products) {
+    const queryString = 'INSERT INTO products (product_name, image_path, department) VALUES(?, ?, ?);';
+    const params = [product.productName, product.productImage, product.department];
+    const insertedBatch = await connection.query(queryString, params);
+  }
+  connection.end();
+};
 
-//       // for each item in the products prop in that variable
-//       console.log(dataSeed);
-//       console.log(dataSeed.products);
-//       for (let product of dataSeed.products) {
-//         const queryString = 'INSERT INTO products (product_name, image_path, department) VALUES(?, ?, ?);';
-//         const params = [product.name, product.image_path, product.department];
-//         console.log('HERES THE IMAGE PATH: ' + params[1]);
-//         console.log(product.name);
-//         console.log(params);
-//         resolve(db.query(queryString, params))
-//          .then((result) => {
-
-//       })
-//       }catch(error => {
-//         console.error(error);
-//       })
-//     // insert into the rateable_attributes table of reviewsDB a row with attribute_name set to the item's prodAdj
-
-//   // insert into users table of reviewsDB a row with username set to the item's
-// };
-
-// seedFakeData = util.promisify(seedFakeData);
-
-
-
-// seedFakeData(1)
-//   .then(db.end());
-
-// CREATE TABLE products (
-//   id int AUTO_INCREMENT PRIMARY KEY,
-//   product_name varchar(120),
-//   image_path varchar(120),
-//   department varchar(50)
-// );
-
-// CREATE TABLE rateable_attributes (
-//   id int AUTO_INCREMENT PRIMARY KEY,
-//   attribute_name varchar(50)
-// );
-
-// CREATE TABLE users (
-//   id int NOT NULL AUTO_INCREMENT,
-//   username varchar(50)
-// );
-
-// CREATE TABLE images (
-//   id int NOT NULL AUTO_INCREMENT,
-//   loc varchar(100)
-// );
-
-// CREATE TABLE reviews (
-//   id int AUTO_INCREMENT,
-//   user_id smallint,
-//   star_rating tinyint,
-//   helpfulness_score tinyint,
-//   image_id int,
-//   PRIMARY KEY (id),
-//   FOREIGN KEY user_id REFERENCES users(id),
-//   FOREIGN KEY image_id REFERENCES images(id)
-// );
+seedFakeData(50);
 
 // prolly just serve up static files with S3
