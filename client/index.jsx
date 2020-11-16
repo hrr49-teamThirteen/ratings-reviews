875ad0d2bf05364622e7750df7108a5cc8792722
Sam Goldie <samuelmgoldie@gmail.com>
2020-11-16 15:34:01 -0800

    Finish index.jsx skeleton

--- client/index.jsx
+++ client/index.jsx
@@ -1,17 +1,53 @@
-const React = require('react');
-const axios = require('axios');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Axios from 'axios';
+
+import getProduct from './getProduct.js';
+import ReviewHead from './components/ReviewHead.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      reviews: []
+      product: null,
+      reviews: [],
+      images: []
     };
   }
 
-  get() {
-    axios.get('');
+  async getProduct() {
+    const response = await axios.get('/products');
+    this.state.product = response.data;
+  }
+
+  // getReviews() {
+  //   axios.get('');
+  // }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <ReviewHead
+            product = {this.state.product}
+            reviews = {this.state.reviews}
+          />
+        </div>
+        <div>
+          <ImageCarousel
+            images = {this.state.images}
+          />
+        </div>
+        <div>
+          <ReviewFeed
+            product = {this.state.product}
+            reviews = {this.state.reviews}
+            images = {this.state.images}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
