0126132745527675ed4a3f0d37ae9728aad5ad28
Sam Goldie <samuelmgoldie@gmail.com>
2020-11-25 15:53:09 -0800

    Fixed dimensions of review images in carousel

--- client/src/components/Image/Image.jsx
+++ client/src/components/Image/Image.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import styles from './Image.module.css';
 
 const Image = ({image}) => {
-  return (<img className={styles.image} src={image.loc} />);
+  return (<div className={styles.imgContainer}>
+    <img className={styles.image} src={image.loc} />
+  </div>);
 };
 
 export default Image;
