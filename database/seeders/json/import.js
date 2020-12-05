f6e322b2131fba8a633bbcbbec8e4d35e9a1ac24
jwnukoski <jwnukoski@protonmail.com>
2020-12-05 17:42:17 -0500

    json import script

--- database/seeders/json/import.js
+++ database/seeders/json/import.js
@@ -1,1 +1,8 @@
-const baseCsvDir = './database/seeders/json/';
+const baseJsonDir = './database/seeders/json/';
+const shell = require('shelljs');
+
+shell.cd(baseJsonDir);
+shell.exec('mongoimport --db reviewsdb --collection products --file products.json');
+shell.exec("mongo --eval 'db.products.createIndex({product_id: -1});' reviewsdb");
+shell.exec('mongoimport --db reviewsdb --collection users --file users.json');
+shell.exec("mongo --eval 'db.users.createIndex({user_id: -1});' reviewsdb");
