25078720351d086a82c4e004a59e6cfeaae75b71
jwnukoski <jwnukoski@protonmail.com>
2020-12-08 19:29:49 -0500

    - newrelic - artillery

--- database/seeders/json/schema.js
+++ database/seeders/json/schema.js
@@ -1,12 +1,13 @@
 // Schemas:
-/*const usersSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
   user_id: {type: Number, index: {unique: true}},
   name:    String
 });
 const userModel = mongoose.model('Users', usersSchema);
 
 const productsSchema = new mongoose.Schema({
-    product_id:   {type: Number, index: {unique: true}},
-    product_name: String,
-    reviews:      [{title: String, date: String, date: String, body: String, star_rating: Number, user_id: Number, images: [String]}]
-});/*
+    product_id:     {type: Number, index: {unique: true}},
+    product_name:   String,
+    reviews:        [{review_id: {type: Number, index{unique: true}}, title: String, date: String, date: String, body: String, star_rating: Number, user_id: Number,
+    images:         [{image_id: {type: Number, index{unique: true}}, loc: String}]
+});
